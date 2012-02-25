cada90184c8c41db165c9385c0f7b6cdfe1d1dca
Óscar González Fernández <ogfernandez@gmail.com>
2012-02-25 21:32:47 +0100

    Image for details of an hotel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,8 +13,17 @@ $(function () {
   $.datepicker.setDefaults($.datepicker.regional['es']);
   $('#start-date').add('#end-date').datepicker({defaultDate: "+1d"});
   $("#travellers-picker").spinner({min: 1, max: 100, increment: 1});
-
   $('#start-date').add('#end-date').change(function() {
     executeLater(checkDatesFilled);
+  });
+  var hotelPicker = $('.hotel-picker');
+  var hotelBooking = $('.hotel-booking');
+  hotelPicker.click(function() {
+    hotelPicker.hide();
+    hotelBooking.show();
+  });
+  hotelBooking.click(function() {
+    hotelBooking.hide();
+    hotelPicker.show();
   });
 });
