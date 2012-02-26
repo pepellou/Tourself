70bde924384f602a520eb938778e450c1690413b
Óscar González Fernández <ogfernandez@gmail.com>
2012-02-26 04:32:12 +0100

    Javascript cleanup

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -53,30 +53,37 @@
 {label:"Kiev, Ucrania", value:"Kiev"},
 {label:"Tashkent, Uzbekistan", value:"Tashkent"}];
 
+$(function () {
+
+  var destinationPicker = $("#destination-picker");
+  destinationPicker.autocomplete({
+    source: availableTowns
+  });
+
+  var travellersPicker = $("#travellers-picker");
+  travellersPicker.spinner({min: 1, max: 100, increment: 1});
+
+  $.datepicker.setDefaults($.datepicker.regional['es']);
+  var datePickers = $('#start-date').add('#end-date');
+  datePickers.datepicker({defaultDate: "+1d"});
+  datePickers.change(function() {
+    executeLater(checkDatesFilled);
+  });
+
+  var hotelPickers = $('.hotel-picker').add('.hotel-booking');
+  hotelPickers.click(function() {
+    hotelPickers.slideToggle();
+  });
+
+});
+
+function executeLater(f) {
+  setTimeout(f, 0);
+}
+
 function checkDatesFilled() {
   if ($('#start-date').val() && $('#end-date').val()) {
     $('#content').show();
   }
 }
 
-function executeLater(f) {
-  setTimeout(f, 0);
-}
-
-$(function () {
-  $('.tabs a:last').tab('show');
-  $.datepicker.setDefaults($.datepicker.regional['es']);
-  $('#start-date').add('#end-date').datepicker({defaultDate: "+1d"});
-  $("#travellers-picker").spinner({min: 1, max: 100, increment: 1});
-  $('#start-date').add('#end-date').change(function() {
-    executeLater(checkDatesFilled);
-  });
-  var hotelPickers = $('.hotel-picker').add('.hotel-booking');
-  hotelPickers.click(function() {
-    hotelPickers.slideToggle();
-  });
-
-  $("#destination-picker" ).autocomplete({
-    source: availableTowns
-  });
-});
